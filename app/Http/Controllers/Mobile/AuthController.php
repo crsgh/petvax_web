3c956b2fcacdf483e1cb608bc0b944c244903880
crsgh <kaloyexo1234@gmail.com>
2025-06-21 14:44:56 +0800

    "Signup default clinic_id = 7"

--- app/Http/Controllers/Mobile/AuthController.php
+++ app/Http/Controllers/Mobile/AuthController.php
@@ -64,7 +64,7 @@ class AuthController extends Controller {
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'clinic_id' => 2,
+            'clinic_id' => 7,
 
         ]);
 
